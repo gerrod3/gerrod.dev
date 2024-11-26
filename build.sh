ae84df533be19149dbef5be6ad62656c4acafb64
Gerrod Ubben <gerrodubben@gmail.com>
2024-11-26 18:26:32 -0500

    Temp fix for build script

--- build.sh
+++ build.sh
@@ -4,3 +4,6 @@
 # Download marmite, run build to output/
 curl -Ls https://github.com/rochacbruno/marmite/releases/download/0.2.1/marmite-0.2.1-x86_64-unknown-linux-musl.tar.gz | tar -xz
 ./marmite source output
+# TODO: Fix this in marmite, specifying your own /static prevents all default assets from being placed
+rm output/favicon.ico output/static/favicon.ico
+touch output/static/favicon.ico
