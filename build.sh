f71786562d5d0df9b598c4eb790d8cfa631d4769
Gerrod Ubben <gerrodubben@gmail.com>
2024-12-22 19:23:39 -0500

    update build script marmite to 0.2.3

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-# TODO: Make this into a makefile
 # Download marmite, run build to output/
-curl -Ls https://github.com/rochacbruno/marmite/releases/download/0.2.1/marmite-0.2.1-x86_64-unknown-linux-musl.tar.gz | tar -xz
-./marmite source output
-# TODO: Fix this in marmite, specifying your own /static prevents all default assets from being placed
-rm output/favicon.ico output/static/favicon.ico
-touch output/static/favicon.ico
+if ! command -v marmite 2>&1 >/dev/null; then
+    echo "hello"
+    curl -Ls https://github.com/rochacbruno/marmite/releases/download/0.2.3/marmite-0.2.3-x86_64-unknown-linux-musl.tar.gz | tar -xz
+    export PATH="$PWD/:$PATH"
+fi
+marmite source output
